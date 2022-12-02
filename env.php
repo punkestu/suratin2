1491bf2deb6c59166ff16b9be8a596626a47eb7d
punkestu <pangestubima89@gmail.com>
2022-12-02 10:05:17 +0700

    change root

--- env.php
+++ env.php
@@ -1,7 +1,7 @@
 <?php
 define("HOST", "0.0.0.0");
-define("USERNAME", "pemro");
-define("PASSWORD", "pemro123");
-define("DB", "suratin");
+define("USERNAME", "tia212410102042");
+define("PASSWORD", "secret");
+define("DB", "uas212410102042");
 define("ROOT", "/tia/tia212410102042/uas");
 define("CONTAINER", __DIR__ . "/container/");
