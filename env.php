1a02237dfb74f917425065b074becb2713bbf380
punkestu <pangestubima89@gmail.com>
2022-12-03 13:11:33 +0700

    create env

--- env.php
+++ env.php
@@ -1,11 +1,11 @@
 <?php
 define("HOST", "localhost");
-// define("USERNAME", "tia212410102042");
-// define("PASSWORD", "secret");
-// define("DB", "uas212410102042");
-// define("ROOT", "/tia/tia212410102042/uas");
-define("USERNAME", "pemro");
-define("PASSWORD", "pemro123");
-define("DB", "suratin");
-define("ROOT", "/suratin2");
+define("USERNAME", "tia212410102042");
+define("PASSWORD", "secret");
+define("DB", "uas212410102042");
+define("ROOT", "/tia/tia212410102042/uas");
+// define("USERNAME", "pemro");
+// define("PASSWORD", "pemro123");
+// define("DB", "suratin");
+// define("ROOT", "/suratin2");
 define("CONTAINER", __DIR__ . "/container/");
