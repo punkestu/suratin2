70de094e6fa63b23cea523b44a5cf1039fb080f9
punkestu <pangestubima89@gmail.com>
2022-12-03 22:21:48 +0700

    still fixing

--- routes/tarik.php
+++ routes/tarik.php
@@ -4,8 +4,7 @@ require_once __DIR__ . "/../env.php";
 
 $conn = new mysqli(HOST, USERNAME, PASSWORD, DB);
 $res = Pengajuan::deleteWhereId($conn, $_GET["code"], $_COOKIE["token"]);
-$res = Notifikasi::push($conn, $_GET["code"], $_COOKIE["token"], "menghapus pengajuan");
-var_dump($res);
+var_dump(Notifikasi::push($conn, $_GET["code"], $_COOKIE["token"], "menghapus pengajuan"));
 
 header("Location: " . ROOT . "/view/listpengajuan.php", true, 301);
 exit();
