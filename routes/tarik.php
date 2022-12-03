08056094df5634414ad774f5aa5bf1aed3e303b6
punkestu <pangestubima89@gmail.com>
2022-12-03 22:24:50 +0700

    debug

--- routes/tarik.php
+++ routes/tarik.php
@@ -1,10 +1,11 @@
 <?php
 require_once __DIR__ . "/../model/pengajuan_model.php";
+require_once __DIR__ . "/../model/notifikasi_model.php";
 require_once __DIR__ . "/../env.php";
 
 $conn = new mysqli(HOST, USERNAME, PASSWORD, DB);
 $res = Pengajuan::deleteWhereId($conn, $_GET["code"], $_COOKIE["token"]);
-var_dump(Notifikasi::push($conn, $_GET["code"], $_COOKIE["token"], "menghapus pengajuan"));
+Notifikasi::push($conn, $_GET["code"], $_COOKIE["token"], "menghapus pengajuan");
 
 header("Location: " . ROOT . "/view/listpengajuan.php", true, 301);
 exit();
