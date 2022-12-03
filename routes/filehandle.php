9c01762dfa08b95c93d2cbf0c8791975efb48627
punkestu <pangestubima89@gmail.com>
2022-12-03 16:26:09 +0700

    debug

--- routes/filehandle.php
+++ routes/filehandle.php
@@ -4,7 +4,8 @@ require_once __DIR__ . "/../model/pengajuan_model.php";
 require_once __DIR__ . "/../model/notifikasi_model.php";
 
 echo CONTAINER;
-var_dump($_FILES['surat']);
+var_dump($_POST);
+var_dump($_FILES);
 if (0 < $_FILES['surat']['error']) {
       echo json_encode(['Error' => $_FILES['file']['error']]);
       exit();
