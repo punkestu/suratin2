e7d2b03eb44fd317f5b79267e9b481c2a45dcf98
punkestu <pangestubima89@gmail.com>
2022-12-04 15:04:38 +0700

    debug

--- routes/post_comment.php
+++ routes/post_comment.php
@@ -5,5 +5,5 @@ require_once __DIR__ . "/../model/komentar_model.php";
 $conn = new mysqli(HOST, USERNAME, PASSWORD, DB);
 $res = Komentar::create($conn, $_POST["message"], $_COOKIE["token"], $_POST["pengajuan"]);
 echo json_encode($res);
-Notifikasi::push($conn, $_POST["pengajuan"], $_COOKIE["token"], "memberi komentar");
 exit();
+Notifikasi::push($conn, $_POST["pengajuan"], $_COOKIE["token"], "memberi komentar");
